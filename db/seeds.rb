ab4cbae8cc2512dac0f330e771a8cdc71461b451
Joshua <joycejosh7@gmail.com>
2021-06-01 11:53:29 -0400

    updated seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Service.create(title: "Plumbing", contractor: "Bob", company: "Bob's Plumbing", description: "Bob did some plumbing", date: Date.today)
-Service.create(title: "Air Conditioner", contractor: "Gary", company: "Gary's Heating and Air", description: "Gary serviced the A/C", date: Date.today)
-Service.create(title: "Painting", contractor: "Susan", company: "Susan's House Painting", description: "Susan painted the bedrooms", date: Date.today)
-Service.create(title: "Deck Maintenance", contractor: "Pat", company: "Pat's Woodworking", description: "Pat resurfaced the deck", date: Date.today)
+Service.create(title: "Plumbing", contractor: "Bob", company: "Bob's Plumbing", description: "Bob did some plumbing", date: Date.today.to_s(:long))
+Service.create(title: "Air Conditioner", contractor: "Gary", company: "Gary's Heating and Air", description: "Gary serviced the A/C", date: Date.today.to_s(:long))
+Service.create(title: "Painting", contractor: "Susan", company: "Susan's House Painting", description: "Susan painted the bedrooms", date: Date.today.to_s(:long))
+Service.create(title: "Deck Maintenance", contractor: "Pat", company: "Pat's Woodworking", description: "Pat resurfaced the deck", date: Date.today.to_s(:long))
